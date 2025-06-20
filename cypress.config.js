53858f4a9942f4e3a1c6cf0e56909abcd84b2dc3
saurav-kushwaha <sauravkushwaha2219@gmail.com>
2025-06-20 19:03:10 +0530

    add video true

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     reporter: 'cypress-mochawesome-reporter',
-
+    
     baseUrl: "https://naveenautomationlabs.com/opencart/index.php?route=account/login",
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
@@ -13,7 +13,8 @@ module.exports = defineConfig({
   },
   env: {
     URL: 'https://naveenautomationlabs.com/opencart/index.php?route=account/register'
-  }
+  },
+  video : true
   //viewportWidth:356,
   //viewportHeight:200
   
