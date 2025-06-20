8eee79722c61d8fc6819de3d9b7063e73c4dfaf1
saurav-kushwaha <sauravkushwaha2219@gmail.com>
2025-06-20 14:07:49 +0530

    add uncaught exception method

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -15,3 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // return false to prevent Cypress from failing the test
+  return false;
+});
