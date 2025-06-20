b179812d4992cf59c89b6b1d71252e4a889bcd0b
saurav-kushwaha <sauravkushwaha2219@gmail.com>
2025-06-20 18:43:39 +0530

    import html report

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -19,3 +19,4 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   // return false to prevent Cypress from failing the test
   return false;
 });
+import 'cypress-mochawesome-reporter/register';
